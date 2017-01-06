00be5d702132c79cd963571f83269c7fd0907ef6
Phvash <davash001@gmail.com>
2017-01-06 20:03:04 +0100

    updated audio

--- tools/audio.py
+++ tools/audio.py
@@ -1,24 +1,118 @@
 import sounddevice as sd
 import numpy as np
+import socket
+import threading
 import time
 
-# duration = 10 # in seconds, must be int
-# fs = 48000 # 44100 or 48000 frames per second
+
+class Player:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def record(duration=5, fs=48000, channels=2, blocking=True):
+        raw_rec = sd.rec(duration * fs,
+                         samplerate=fs, channels=channels, blocking=blocking)
+        return raw_rec.tostring()
+
+    @staticmethod
+    def play(string_data, fs=48000, data_type='float32', blocking=True):
+        raw_rec = np.fromstring(string_data, data_type)
+        sd.play(raw_rec, fs, blocking=blocking)
 
 
-def makeRecording(duration=10, fs=48000):
-	raw_vr = sd.rec(duration * fs, samplerate=fs, channels=2)
-	sd.wait()
-	return raw_vr.tostring()
+class SendAud(threading.Thread):
+    def __init__(self, target_ip, port):
+        threading.Thread.__init__(self)
+        self.target_ip = target_ip
+        self.port = port
+        # self.aud_str = Player.record()
 
-def playRecording(string_data, fs=48000, data_type = 'float32'):
-	reconstructed_data = np.fromstring(string_data, data_type)
-	sd.play(reconstructed_data, fs)
-	sd.wait()
+    def run(self):
+        while True:
+            aud_str = Player.record()
+            s = socket.socket()
+            print "connection to " + self.target_ip + 'on' + str(self.port)
+            s.connect((self.target_ip, self.port))
+            s.sendall(aud_str)
+            s.close()
 
-def test():
-	a = makeRecording()
-	playRecording(a)
+
+class RecvAud(threading.Thread):
+    def __init__(self, port):
+        threading.Thread.__init__(self)
+        self.host = ''
+        self.port = port
+        self.playlist = []
+        self.lock = threading.Lock()
+
+    def run(self):
+
+        self.recv_thread = threading.Thread(target=self.receive_audio,  name='recv_aud')
+        self.recv_thread.start()
+
+        self.proc_thread = threading.Thread(target=self.process_audio,  name='proc_aud')
+        self.proc_thread.start()
+
+    def receive_audio(self):
+        while True:
+            s = socket.socket()
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((self.host, self.port))
+            print "Listening on port", self.port
+            s.listen(1)
+            conn, addr = s.accept()
+            print 'connection from', addr
+
+            received = []  # contains aud_string received in one conn
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                else:
+                    received.append(data)
+                    # print "received data"
+            aud_str = ''.join(received)
+            print 'rebuilt string'
+            # self.lock.acquire()
+            print "acquire lock"
+            self.playlist.append(aud_str)
+            print "added str to playlist"
+            # self.lock.release()
+            print "released lock"
+            print "appended"
+
+    def process_audio(self):
+        while True:
+            try:
+                print "got here"
+                # acquired = self.lock.acquire(0)
+                if len(self.playlist) > 0:
+                    current_track = self.playlist[0]
+                else:
+                    time.sleep(5)
+                    continue
+                self.playlist.remove(current_track)
+                # self.lock.release()
+                if True:
+                    Player.play(current_track)
+                    print "played"
+                else:
+                    pass
+            finally:
+                if not self.recv_thread.isAlive():
+                    break
 
 if __name__ == '__main__':
-	test()
+    # a = Player.record()
+    # Player.play(a)
+
+    queue = []
+    recv_thread = RecvAud(7079)
+    send_thread = SendAud('127.0.0.1', 7079)
+    queue.append(recv_thread)
+    queue.append(send_thread)
+    recv_thread.start()
+    send_thread.start()
+    for thread in queue:
+        thread.join()
--- tools/call.py
+++ tools/call.py
@@ -5,80 +5,81 @@ import socket
 
 
 class Call:
-	def __init__(self, vid_recv_port, vid_send_port, client_ip='localhost'):
-		self.client_ip = client_ip
-		self.job_queue = []
-		self.vid_recv_port = vid_recv_port
-		self.vid_send_port = vid_send_port
-	def start(self):
-		svf_obj = send_video_feed(self.vid_send_port, self.client_ip)
-		self.job_queue.append(svf_obj)
-		rvf_obj = receive_video_feed(self.vid_recv_port)
-		self.job_queue.append(rvf_obj)
-		svf_obj.start()
-		rvf_obj.start()
-		# wait till the jobs are terminated
-		for job in self.job_queue:
-			job.join()
+    def __init__(self, vid_recv_port, vid_send_port, client_ip='localhost'):
+        self.client_ip = client_ip
+        self.job_queue = []
+        self.vid_recv_port = vid_recv_port
+        self.vid_send_port = vid_send_port
 
+    def start(self):
+        svf_obj = send_video_feed(self.vid_send_port, self.client_ip)
+        self.job_queue.append(svf_obj)
+        rvf_obj = receive_video_feed(self.vid_recv_port)
+        self.job_queue.append(rvf_obj)
+        svf_obj.start()
+        rvf_obj.start()
+        # wait till the jobs are terminated
+        for job in self.job_queue:
+            job.join()
 
 
 class send_video_feed(threading.Thread):
-	def __init__(self, vid_send_port, target_ip='localhost'):
-		threading.Thread.__init__(self)
-		self.target_ip = target_ip
-		self.port = vid_send_port
-		self.cap = cv2.VideoCapture(0)
-	    
-	def run(self):
-	    while True:
-	        ret, frame = self.cap.read()
-	        rval, imgencode = cv2.imencode(".jpg", frame, [1, 90])
-	        data = numpy.array(imgencode)
-	        stringData = data.tostring()
+    def __init__(self, vid_send_port, target_ip='localhost'):
+        threading.Thread.__init__(self)
+        self.target_ip = target_ip
+        self.port = vid_send_port
+        self.cap = cv2.VideoCapture(0)
 
-	        s = socket.socket()
-	        s.connect((self.target_ip, self.port))
-	        s.sendall(stringData)
+    def run(self):
+        while True:
+            ret, frame = self.cap.read()
+            rval, imgencode = cv2.imencode(".jpg", frame, [1, 90])
+            data = numpy.array(imgencode)
+            stringData = data.tostring()
 
-	    s.close()
+            s = socket.socket()
+            s.connect((self.target_ip, self.port))
+            s.sendall(stringData)
+
+        s.close()
+
 
 class receive_video_feed(threading.Thread):
-	def __init__(self, vid_recv_port):
-		threading.Thread.__init__(self)
-		self.port = vid_recv_port
+    def __init__(self, vid_recv_port):
+        threading.Thread.__init__(self)
+        self.port = vid_recv_port
 
+    def run(self):
+        while True:
+            host = ''
+            port = self.port
+            s = socket.socket()
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((host, port))
+            s.listen(1)
+            conn, addr = s.accept()
+            s_ip, s_port = addr
 
-	def run(self):	
-		while True:
-			host = ''
-			port = self.port
-			s = socket.socket()
-			s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-			s.bind((host, port))
-			s.listen(1)
-			conn, addr = s.accept()
-			s_ip, s_port = addr
-			
-			message = []
-			
-			while True:
-				data = conn.recv(1024 * 1024)
-				if not data:
-					break
-				else:
-					message.append(data)
-			data = ''.join(message)
-			string_data = numpy.fromstring(data, numpy.uint8)
+            message = []
 
-			decimg = cv2.imdecode(string_data, 1)
-			cv2.imshow("Bello call from " + s_ip, decimg)
+            while True:
+                data = conn.recv(1024 * 1024)
+                if not data:
+                    break
+                else:
+                    message.append(data)
+            data = ''.join(message)
+            string_data = numpy.fromstring(data, numpy.uint8)
 
-			if cv2.waitKey(5) == 27:
-				break
-		cv2.destroyAllWindows()
+            decimg = cv2.imdecode(string_data, 1)
+            cv2.imshow("Bello call from " + s_ip, decimg)
+
+            if cv2.waitKey(5) == 27:
+                break
+        cv2.destroyAllWindows()
+
 
 # quick test
 if __name__ == '__main__':
-	call_obj = Call()
-	call_obj.start()
+    call_obj = Call(5610, 6532)
+    call_obj.start()
